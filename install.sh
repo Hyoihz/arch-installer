ebbb2f21ee921716f0574158ab81336e30f05227
Raynald Escala <qwertyraynald@gmail.com>
2023-02-26 17:46:10 +0000

    Add comments

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ prompt_input() {
 }
 
 is_uefi_boot() {
+    # If the directory exists, system is using UEFI
     [[ -d /sys/firmware/efi/ ]] && return 0 || return 1
 }
 
@@ -99,7 +100,7 @@ set_partition_vars() {
         BOOT_MTPT="/mnt/boot"
         BOOT_PARTITION="${partition_block_path}1"
     fi
-
+    # Both
     ROOT_PARTITION="${partition_block_path}2"
     SWAP_PARTITION="${partition_block_path}3"
 }
@@ -107,16 +108,23 @@ set_partition_vars() {
 read_partition_size() {
     clear
     while true; do
+	# Get the available space for the partition
         display_info "Available space: $(numfmt --to=iec --format='%.1f' "$2")"
+	# Prompt the user to enter the partition size
         prompt_input "$1"
         read -r size
+
+	# Validate the partition size format and available space
         if [[ "$size" =~ ^[0-9]+[KMGT]$ ]]; then
             size_bytes=$(numfmt --from=iec "$size")
             if [[ "$size_bytes" -le 0 ]]; then
+		# Display an error if the partition size is invalid
                 clear && display_error "Invalid size. Please specify a valid size." && echo
             elif [[ "$size_bytes" -gt "$2" ]]; then
+		# Display an error if the partition size is larger than the available space
                 clear && display_error "Not enough available space. Please specify a size smaller than $(numfmt --to=iec --format='%.1f' "$2")." && echo
             else
+		# Assign the partition size to the appropriate variable and update the available space
                 if [[ $3 == "boot_size" ]]; then
                     boot_size=$size
                 else
@@ -126,6 +134,7 @@ read_partition_size() {
                 available_space=$(($2 - size_bytes)) && break
             fi
         else
+            # Display an error if the partition size is larger than the available space
             clear && display_error "Invalid size. Please specify a size in the format [0-9]+[KMG] (e.g. 512M)." && echo
         fi
     done
@@ -135,6 +144,7 @@ confirm_partition_sizes() {
     clear display_info "Consuming remaining size for root..." && sleep 1
     display_info "Assigned partition sizes: " && echo
 
+    # Display the sizes of the EFI/BOOT, SWAP, and ROOT partitions
     if $(is_uefi_boot); then
         display_info "EFI partition size: $boot_size"
     else
@@ -144,6 +154,7 @@ confirm_partition_sizes() {
     display_info "SWAP partition size: $swap_size"
     display_info "ROOT partition size: $(numfmt --to=iec --format='%.1f' "$available_space")"
 
+    # Prompt the user to confirm the partition sizes
     prompt_input "Are you satisfied with these partition sizes? (Y/n) "
     read -r choice
     while [[ "$choice" != "y" && "$choice" != "n" && "$choice" != "" ]]; do
@@ -151,12 +162,15 @@ confirm_partition_sizes() {
         read -r choice
     done
 
+    # Return 1 if the user does not confirm the partition sizes, 0 otherwise
     [[ "$choice" == "n" ]] && return 1 || return 0
 }
 
 set_partition_sizes() {
+    # Determine available space on the device
     available_space=$(lsblk -nb -o SIZE -d "$partition_block_path" | tail -1)
 
+    # Prompt the user to enter the size of the boot partition (UEFI or BIOS)
     if $(is_uefi_boot); then
         prompt="Enter EFI partition size (e.g. 512M): "
     else
@@ -165,9 +179,11 @@ set_partition_sizes() {
 
     read_partition_size "$prompt" "$available_space" "boot_size"
 
+    # Prompt the user to enter the size of the swap partition
     prompt="Enter SWAP partition size (e.g. 4G): "
     read_partition_size "$prompt" "$available_space" "swap_size"
 
+    # Confirm the partition sizes with the user and give the option to modify them if needed
     confirm_partition_sizes
     [[ $? -ne 0 ]] && set_partition_sizes
 }
@@ -181,22 +197,26 @@ mount_partition() {
 }
 
 create_partitions() {
+    # Creates partitions on the block device, formats them, and mounts them.
     if $(is_uefi_boot); then
+        # Create GPT partition table and partitions for UEFI boot.
         sgdisk -Z "$partition_block_path"
         sgdisk -n 1::+"$boot_size" -t 1:ef00 -c 1:EFI "$partition_block_path"
         sgdisk -n 2::+"$swap_size" -t 2:8200 -c 2:SWAP "$partition_block_path"
         sgdisk -n 3 -t 3:8300 -c 3:ROOT "$partition_block_path"
 
-        # Format
+        # Format partitions
         mkfs.fat -F32 "$EFI_PARTITION"
         format_partition "$ROOT_PARTITION" "ext4"
         mkswap "$SWAP_PARTITION"
-        # Mount
+
+        # Mount partitions
         mkdir -p "$EFI_MTPT"
         mount_partition "$EFI_PARTITION" "$EFI_MTPT"
         mount_partition "$ROOT_PARTITION" /mnt
         swapon "$SWAP_PARTITION"
     else
+	# Create partitions for BIOS boot.
         cat >/tmp/sfdisk.cmd <<EOF
 $BOOT_PARTITION : start= 2048, size=+$boot_size, type=83, bootable
 $SWAP_PARTITION : size=+$swap_size, type=82
@@ -204,11 +224,12 @@ $ROOT_PARTITION : type=83
 EOF
         sfdisk "$partition_block_path" </tmp/sfdisk.cmd
 
-        # Format
+        # Format partitions
         format_partition "$BOOT_PARTITION" "ext4"
         format_partition "$ROOT_PARTITION" "ext4"
         mkswap "$SWAP_PARTITION"
-        # Mount
+
+        # Mount partitions
         mkdir -p "$BOOT_MTPT"
         mount_partition "$BOOT_PARTITION" "$BOOT_MTPT"
         mount_partition "$ROOT_PARTITION" /mnt
@@ -222,14 +243,20 @@ set_password() {
     while true; do
         prompt_input "Enter a password for $1: "
         read -r -s password
+
+	# Check if the password is empty
         if [[ -z "$password" ]]; then
             clear && display_error "You need to enter a password, try again."
         else
+	    # Prompt the user to confirm the password
             echo && prompt_input "Enter the password again: "
             read -r -s password2 && echo
+	    
+	    # Check if the two passwords match
             if [[ "$password" != "$password2" ]]; then
                 clear && display_error "Passwords don't match, try again."
             else
+	        # Store the password as root password or user password
                 if [[ $1 == "root" ]]; then
                     root_pass=$password
                 else
@@ -246,17 +273,21 @@ create_user_account() {
         prompt_input "Enter username (leave blank ito not create one): "
         read -r username
 
+	# Check if the user already exists
         if id "$username" >/dev/null 2>&1; then
             clear && display_error "User $username already exists, try again." && continue
         fi
-
+ 
+	# If the username is empty, exit the loop
         [[ -z "$username" ]] && break
+
         set_password "$username"
 
-        # Create the user account.
+        # Create the user account
         useradd -m "$username" >/dev/null 2>&1 || {
             display_error "Failed to create an account for '$username'." && continue
         }
+	# Set the password for the user account
         echo "$username:$user_pass" | chpasswd || {
             display_error "Failed to set a password for '$username'." && continue
         }
@@ -267,9 +298,9 @@ create_user_account() {
 }
 
 set_root_password() {
-    # Prompt for the root password.
     clear && display_info "Set a root password." && echo
 
+    # Set the root password
     echo "root:$root_pass" | chpasswd || {
         display_error "Failed to set root password."
     }
